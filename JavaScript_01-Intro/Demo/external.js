b109be1c8bec1fbb3766026a58ffeab02d6c1227
JelenaM <jelena.miodragovic@seavus.com>
2023-03-18 13:04:30 +0100

    JavaScript_01-Intro

---
+++ JavaScript_01-Intro/Demo/external.js
@@ -0,0 +1,43 @@
+// This is a string
+var firstName = "Marko";
+var lastName = "Markovic";
+//console.log(firstName, lastName);
+//console.log("Ovo je ime i prezime: ", firstName, lastName);
+// This is a number
+let first = 1;
+//console.log(first);
+//console.log("Ovo je broj", first);
+// This is also a string
+let age = "24";
+// This is a number
+let numberAge = 24;
+// This is a number
+let decimalNumber = 2.5;
+// This is a boolean
+let knowsHtml = true;
+let knowJS = false;
+//*************************
+let stringExample = "Ivan";
+let numberExample = 100;
+let booleanExample = true;
+let undefinedExample;
+let nullExample = null;
+// console.log(typeof stringExample);
+// console.log(typeof numberExample);
+// console.log(typeof booleanExample);
+// console.log(typeof undefinedExample);
+// console.log(typeof nullExample);
+//*************************
+let a = 1;
+let b = 2;
+//console.log(a < b); //true
+let c = 2;
+//console.log((c += b)); //3 c=c+b c=1+2
+
+let userInput = prompt("Enter length in feet:");
+console.log(userInput);
+//TIP: 1 feet = 0.3048 meters
+
+// var conversionFactor = 0.3048;
+// var lengthInFeet = 15;
+// var convertedValue = lengthInFeet * conversionFactor;
